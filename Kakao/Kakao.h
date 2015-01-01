979527f9d25f4cacf18fe2040eca38c57e150220
choiks14 <ceo@sun-studio.co.kr>
2015-01-01 15:21:35 +0900

    first commit

---
+++ Kakao/Kakao.h
@@ -0,0 +1,13 @@
+//
+//  Kakao.h
+//  Kakao
+//
+//  Created by choikangsik on 2015. 1. 1..
+//  Copyright (c) 2015년 choikangsik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Kakao : NSObject
+
+@end
---
+++ Kakao/Kakao.m
@@ -0,0 +1,13 @@
+//
+//  Kakao.m
+//  Kakao
+//
+//  Created by choikangsik on 2015. 1. 1..
+//  Copyright (c) 2015년 choikangsik. All rights reserved.
+//
+
+#import "Kakao.h"
+
+@implementation Kakao
+
+@end
